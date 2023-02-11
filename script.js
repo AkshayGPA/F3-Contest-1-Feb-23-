65218b286b2b4995dcda4e11c433a07a08f77874
akshay_gaikwad <akshay240695@gmail.com>
2023-02-11 19:59:34 +0530

    Project complete

--- script.js
+++ script.js
@@ -1,14 +1,93 @@
-console.log("Product list call from API")
+// console.log("Product list call from API")
+// let container = document.getElementsByClassName("container");
 
-fetch("https://dummyjson.com/products")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log("DATA>>>", data);
-        let prodList = data.products;
-        let prodDiv = document.getElementById("productList");
+// fetch("https://dummyjson.com/products")
+//     .then((response) => response.json())
+//     .then((data) => {
+//         console.log("DATA>>>", data);
+//         let prodList = data.products;
+//         let prodDiv = document.getElementById("productList");
 
-        // prodList.map((products) => {
-        //     prodDiv.innerHTML += `<p>${products.title}</p>`;
-        // });
+//         // prodList.map((products) => {
+//         //     prodDiv.innerHTML += `<p>${products.title}</p>`;
+//         // });
+//         for (let i = 0; i < prodList.length ; i++){
+//             const element = prodList[i];
+//             let categoryy = element.category;
+//             let idd = element.id;
+//             let brandd = element.brand;
+//             let titlee = element.title;
+//             let descriptionn = element.description;
+//             let sellingPrice = element.price;
+//             let discount = element.discountPercentage;
+//             let availability = element.stock;
+//             let prodReview = element.rating;
+//             let imageArr = element.images;
+//             let imagee = imageArr[0];
+            
+//             let flexCard = document.createElement("span");
 
+//             flexCard.innerHTML = 
+//             `<div class="flexCard">
+//                 <img class="img" src="${imagee}">
+//                 <p class="prodTitle">Title: ${titlee}</p>
+//                 <p class="brand">Brand: ${brandd}</p>
+//                 <p class="cost">${sellingPrice}</p>
+//                 <p class="rating">Rating : ${prodReview}</p>
+//                 <p class="inStock">Stock : ${availability}</p>
+//                 <p class="description">${descriptionn}</p>
+//                 <p class="rebate">Stock : ${discount}</p>
+//             </div>`
+
+//             container.appendChild(flexCard);
+
+//         }
+
+
+
+// });
+
+
+
+
+
+
+
+fetch("https://dummyjson.com/products").then((response)=> response.json()).then((data)=>{
+    let primeData = data.products;
+    let divv = document.getElementById('container');
+    console.log(divv); // this stores the main div element where we gonaa display our cards
+ 
+  // to loop over a primeData array of objects
+  for (let i = 0; i < primeData.length; i++) {
+    const element = primeData[i];
+    let categoryy = element.category;
+    let idd = element.id;
+    let brandd = element.brand;
+    let titlee = element.title;
+    let descriptionn = element.description;
+    let pricee = element.price;
+    let discountPercentagee = element.discountPercentage;
+    let stockk = element.stock;
+    let ratingg = element.rating;
+    let imageArr = element.images;
+    let imagee = imageArr[0];
+    console.log(`iteration ${i}`)
+    console.log(element, categoryy, idd, brandd, titlee, descriptionn, pricee, discountPercentagee, stockk, ratingg, imagee);
+    let spann = document.createElement("span");
+    console.log('Hello');
+    spann.innerHTML = 
+    `<div class="flexCard" >
+        <img class="img" src="${imagee}" height="60%" width="100%"><br>
+        <p class="prodTitle"><b>Title:</b> ${titlee}</p>
+        <p class="brand"><b>Brand:</b> ${brandd}</p>
+        <p class="cost"><b>Cost:</b> ${pricee}</p>
+        <p class="rating"><b>Rating:</b> ${ratingg}</p>
+        <p class="inStock"><b>Stock:</b> ${stockk}</p>
+        <p class="discountpercentage"><b>Rebate:</b> ${discountPercentagee}%</p>
+        <p class="description">${descriptionn}</p>
+    </div>`
+        divv.appendChild(spann);
+        // divv.appendChild(spann);
+    }
 });
